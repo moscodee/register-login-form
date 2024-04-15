6439a8dfb8011a697c93b0db62bae40de5c48c51
moscode <moscodeprog@gmail.com>
2024-04-15 13:47:45 +0330

    Update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 //welcome
 //color site => https://colorhunt.co/palette/b3c8cfbed7dcf1eedce5ddc5
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
+import Panel from "./components/Panel";
 function App() {
   return (
     <>
-      <h1>hi</h1>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/panel/:username?" element={<Panel />} />
+          </Routes>
+        </Layout>
+      </Router>
     </>
   );
 }
