f6b0751abb38c8b2aceef1590e6ca07a4de7725b
moscode <moscodeprog@gmail.com>
2024-04-15 13:39:03 +0330

    git init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//welcome
+//color site => https://colorhunt.co/palette/b3c8cfbed7dcf1eedce5ddc5
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+}
+
+export default App;
