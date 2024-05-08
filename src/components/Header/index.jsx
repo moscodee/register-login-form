49638b07ee18db649bc96325e86c49484cfd7e48
moscode <moscodeprog@gmail.com>
2024-05-08 12:28:07 +0330

    Add: dropdown and style it

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import CSS from "../CSS/Header.module.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 const Header = () => {
+  const [isShow, setIsShow] = useState(false);
   const navigate = useNavigate();
+  const { register } = useParams();
+  console.log(register);
+  const handleDropDown = () => {
+    setIsShow(!isShow);
+  };
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 650) {
+      setIsShow(false);
+    }
+  });
+
   return (
     <>
       <div className={CSS.navbar_container}>
@@ -17,9 +30,33 @@ const Header = () => {
           <button className={CSS.btn} onClick={() => navigate("/register")}>
             Register
           </button>
-          <button className={CSS.toggle_navbar} onClick={() => {}}>
-            =
-          </button>
+        </div>
+        <button className={CSS.dropdown} onClick={handleDropDown}>
+          =
+        </button>
+        <div>
+          {isShow && (
+            <div className={isShow ? CSS.dropdown_active : ""}>
+              <button
+                className={CSS.dropdown_item}
+                onClick={() => navigate("/")}
+              >
+                Home
+              </button>
+              <button
+                className={CSS.dropdown_item}
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+              <button
+                className={CSS.dropdown_item}
+                onClick={() => navigate("/register")}
+              >
+                Register
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
